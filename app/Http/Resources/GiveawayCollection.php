668f0044607f4367113c9b86d278a4321f7201ac
Jeremy Anderson <jeremy@edos.io>
2018-10-24 09:33:06 -0600

    fix circleCI image config?

---
+++ app/Http/Resources/GiveawayCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Elbow\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class GiveawayCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
