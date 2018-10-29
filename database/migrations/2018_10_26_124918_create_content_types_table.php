82a6082dfea8b84b63d147e025077996614a2ec3
Jeremy Anderson <jeremy@edos.io>
2018-10-28 20:25:16 -0600

    migrations fix

--- database/migrations/2018_10_26_124918_create_content_types_table.php
+++ database/migrations/2018_10_26_124918_create_content_types_table.php
@@ -16,7 +16,7 @@ class CreateContentTypesTable extends Migration
         Schema::create('content_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('type')->default('basic');
-            $table->json('fields')->default('{}');
+            $table->json('fields')->nullable();
             $table->timestamps();
         });
     }
