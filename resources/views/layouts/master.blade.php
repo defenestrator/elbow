06c95a41f9d0aee5066ec6082397eb6e10258fd0
Jeremy Anderson <jeremy@edos.io>
2018-11-27 15:12:48 -0700

    remove link until fixed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -38,8 +38,8 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
+                            {{--<li><a class="nav-link" href="{{ route('seeds.index') }}">{{ __('Seeds') }}</a></li>--}}
                             <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a class="nav-link" href="{{ route('seeds.index') }}">{{ __('Seeds') }}</a></li>
                             <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                         @else
                             <li class="nav-item dropdown">
