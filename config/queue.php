faaa5efde0ba5f76033a380f141bc89fa5f6383a
Jeremy Anderson <jeremy@edos.io>
2019-01-29 11:48:32 -0700

    Howdy

--- config/queue.php
+++ config/queue.php
@@ -60,7 +60,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
-            'queue' => 'elbow_queue',
+            'queue' => '{elbow_queue}',
             'retry_after' => 90,
             'block_for' => null,
         ],
