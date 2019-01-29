dd5e6100b20d572715132de1df70b936f26c35a8
Jeremy Anderson <jeremy@edos.io>
2019-01-29 11:18:33 -0700

    Howdy

--- config/database.php
+++ config/database.php
@@ -135,6 +135,12 @@ return [
             'port' => env('REDIS_PORT', 6379),
             'database' => 7,
         ],
+        'elbow_broadcast' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 14,
+        ],
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
