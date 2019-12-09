2b3bc372608bcca4a8a9fc5bb6861c7ce77f3d61
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-12-09 13:41:21 -0700

    PotLuck and Shit like that.

---
+++ app/Http/Controllers/PotLuckController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Elbow\Http\Controllers;
+
+use Elbow\PotLuck;
+use Illuminate\Http\Request;
+
+class PotLuckController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Elbow\PotLuck  $potLuck
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PotLuck $potLuck)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Elbow\PotLuck  $potLuck
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PotLuck $potLuck)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Elbow\PotLuck  $potLuck
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PotLuck $potLuck)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Elbow\PotLuck  $potLuck
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PotLuck $potLuck)
+    {
+        //
+    }
+}
---
+++ database/factories/PotLuckFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Elbow\PotLuck;
+use Faker\Generator as Faker;
+
+$factory->define(PotLuck::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
