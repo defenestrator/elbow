52f9e3baffeff4790b28813886b1e9a9a7b6c693
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-01-26 00:35:23 -0700

    better message

--- app/Exceptions/Corruption.php
+++ app/Exceptions/Corruption.php
@@ -6,7 +6,6 @@ use Exception;
 
 class Corruption extends Exception
 {
-    protected $message = 'The model you are trying to retrieve has been corrupted. 
-                        Probably by some DBA editing a production datastore in place. 
-                        Bad DBA, and/or Ops, no pizza! Just kidding, we coders are probably worse.';
+    protected $message = 'This data has been defined as immutable. 
+                        The record you are trying to retrieve has been corrupted.';
 }
