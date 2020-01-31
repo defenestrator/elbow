332a1df76e13c37ff816555e9729473cc2318367
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-01-31 09:20:14 -0700

    dingbat

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,16 +1,10 @@
 
-import App from "./components/App.svelte";
 import Potluck from './components/potluck/Potluck.svelte'
 
-const app = new App({
-  target: document.body.main
-})
 
 const potluck = new Potluck({
   target: banana
 })
-
-window.app = app;
 
 window.potluck = potluck;
 
