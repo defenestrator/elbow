84162336b5e991df15fbc19d31076e2260a37c89
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-02-01 10:02:11 -0700

    styles

--- resources/js/components/potluck/Potluck.svelte
+++ resources/js/components/potluck/Potluck.svelte
@@ -414,7 +414,7 @@ onMount(() => {
 
     </div>
     <div class="container-fluid py-3 px-5 bg-white">
-        Open Developer Tools Console adn click<br> 
+        Open Developer Tools Console and click:  
         <button on:click="{executeTurn}" class="btn-blue">Next Turn</button>
     </div>
 </main>
